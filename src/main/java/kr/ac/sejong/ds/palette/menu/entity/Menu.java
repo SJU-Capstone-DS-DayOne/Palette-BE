7ef7fe844142fc7c9e796f31d1c4c7e439c2e8a7
Sungwoo Cho <seouk77@gmail.com>
2024-04-24 18:01:25 +0900

    feat: Menu 엔티티 작성

--- src/main/java/kr/ac/sejong/ds/palette/menu/entity/Menu.java
+++ src/main/java/kr/ac/sejong/ds/palette/menu/entity/Menu.java
@@ -1,10 +1,8 @@
 package kr.ac.sejong.ds.palette.menu.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import kr.ac.sejong.ds.palette.common.entity.BaseEntity;
+import kr.ac.sejong.ds.palette.restaurant.entity.Restaurant;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,4 +14,17 @@ public class Menu extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    private String imageUrl;
+
+    private Integer rank;
+
+    private int price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
 }
