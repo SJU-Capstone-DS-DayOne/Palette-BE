1fca8746f848e0266347c0f952b2e2c17ee67856
Sungwoo Cho <seouk77@gmail.com>
2024-05-05 13:25:13 +0900

    feat: 로그인 Request DTO 작성

---
+++ src/main/java/kr/ac/sejong/ds/palette/member/dto/request/MemberLoginRequest.java
@@ -0,0 +1,11 @@
+package kr.ac.sejong.ds.palette.member.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class MemberLoginRequest {
+
+    private String email;
+
+    private String password;
+}
