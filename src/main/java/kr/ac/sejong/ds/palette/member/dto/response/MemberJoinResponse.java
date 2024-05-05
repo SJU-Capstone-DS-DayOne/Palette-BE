634d4c757450a6dd9443b5ae55f119bfd396271d
Sungwoo Cho <seouk77@gmail.com>
2024-05-05 12:50:24 +0900

    feat: 회원가입 DTO 작성

---
+++ src/main/java/kr/ac/sejong/ds/palette/member/dto/response/MemberJoinResponse.java
@@ -0,0 +1,12 @@
+package kr.ac.sejong.ds.palette.member.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MemberJoinResponse {
+
+    private final Long id;
+}
