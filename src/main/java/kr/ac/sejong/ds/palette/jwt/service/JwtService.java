ccdaf4039c58a3f6a12cad692976dbaacbc6b821
Sungwoo Cho <seouk77@gmail.com>
2024-05-07 17:07:38 +0900

    !hotfix: cookie null 오류 핸들링

--- src/main/java/kr/ac/sejong/ds/palette/jwt/service/JwtService.java
+++ src/main/java/kr/ac/sejong/ds/palette/jwt/service/JwtService.java
@@ -28,9 +28,11 @@ public class JwtService {
         // get refresh token
         String refresh = null;
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("refresh")) {
-                refresh = cookie.getValue();
+        if(cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("refresh")) {
+                    refresh = cookie.getValue();
+                }
             }
         }
 
