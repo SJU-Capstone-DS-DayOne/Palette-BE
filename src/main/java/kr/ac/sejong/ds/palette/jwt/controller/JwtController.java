131e42de2f0faff9b2b1a4e890c5b12be426ac5a
Sungwoo Cho <seouk77@gmail.com>
2024-05-13 03:32:53 +0900

    feat: 회원 컨트롤러 Swagger 적용

--- src/main/java/kr/ac/sejong/ds/palette/jwt/controller/JwtController.java
+++ src/main/java/kr/ac/sejong/ds/palette/jwt/controller/JwtController.java
@@ -1,19 +1,16 @@
 package kr.ac.sejong.ds.palette.jwt.controller;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import jakarta.servlet.http.Cookie;
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.ac.sejong.ds.palette.jwt.service.JwtService;
 import kr.ac.sejong.ds.palette.jwt.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static kr.ac.sejong.ds.palette.jwt.util.JWTUtil.*;
-
+@Hidden
 @RestController
 @RequiredArgsConstructor
 public class JwtController {
@@ -23,7 +20,6 @@ public class JwtController {
 
     @PostMapping("/reissue")
     public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {  // 인자는 스프링 컨테이너가 자동으로 주입해주는 값
-
         return jwtService.reissue(request, response);
     }
 }
--- src/main/java/kr/ac/sejong/ds/palette/member/controller/MemberController.java
+++ src/main/java/kr/ac/sejong/ds/palette/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package kr.ac.sejong.ds.palette.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.ac.sejong.ds.palette.member.dto.request.MemberJoinRequest;
 import kr.ac.sejong.ds.palette.member.dto.request.MemberNicknameUpdateRequest;
@@ -10,35 +12,35 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "회원 (Member)")
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/members")
-    public void hello(){
-        System.out.println("HI");
-    }
-
+    @Operation(summary = "회원 가입")
     @PostMapping("/join")
     public ResponseEntity<MemberJoinResponse> join(@RequestBody @Valid MemberJoinRequest memberJoinRequest){
         MemberJoinResponse memberJoinResponse = memberService.join(memberJoinRequest);
         return ResponseEntity.ok().body(memberJoinResponse);
     }
 
+    @Operation(summary = "단일 회원 정보 조회")
     @GetMapping("/members/{memberId}")
     public ResponseEntity<MemberResponse> getMember(@PathVariable(name = "memberId") Long memberId){
         MemberResponse memberResponse = memberService.getMember(memberId);
         return ResponseEntity.ok().body(memberResponse);
     }
 
+    @Operation(summary = "회원 닉네임 수정")
     @PutMapping("/members/{memberId}")
     public ResponseEntity<Void> updateMemberNickname(@PathVariable(name = "memberId") Long memberId, @RequestBody @Valid MemberNicknameUpdateRequest memberNicknameUpdateRequest){
         memberService.updateNickname(memberId, memberNicknameUpdateRequest);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "회원 탈퇴")
     @DeleteMapping("/members/{memberId}")
     public ResponseEntity<Void> deleteMember(@PathVariable(name = "memberId") Long memberId){
         memberService.deleteMember(memberId);
