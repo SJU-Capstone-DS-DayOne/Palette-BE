9ccc5bbc573c8f122a7d08b7b3085f6781784126
Sungwoo Cho <seouk77@gmail.com>
2024-05-13 15:01:24 +0900

    2

--- src/main/java/kr/ac/sejong/ds/palette/common/config/SecurityConfig.java
+++ src/main/java/kr/ac/sejong/ds/palette/common/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
                         configuration.setMaxAge(3600L);
 
+                        configuration.setExposedHeaders(Arrays.asList("access"));
 
                         return configuration;
                     }
