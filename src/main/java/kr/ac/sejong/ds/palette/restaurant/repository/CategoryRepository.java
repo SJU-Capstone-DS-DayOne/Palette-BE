b73e21bf66b36aef00fa9535a6cbfbb4bdf09681
Sungwoo Cho <seouk77@gmail.com>
2024-05-21 18:09:40 +0900

    refactor: Category 패키지 -> Restaurant 패키지로 통합

--- src/main/java/kr/ac/sejong/ds/palette/restaurant/repository/CategoryRepository.java
+++ src/main/java/kr/ac/sejong/ds/palette/restaurant/repository/CategoryRepository.java
@@ -1,6 +1,6 @@
-package kr.ac.sejong.ds.palette.category.repository;
+package kr.ac.sejong.ds.palette.restaurant.repository;
 
-import kr.ac.sejong.ds.palette.category.entity.Category;
+import kr.ac.sejong.ds.palette.restaurant.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
