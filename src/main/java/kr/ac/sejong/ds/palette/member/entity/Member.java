efb2fdd05dd0320cf3ea759197f6bbf77c89bbff
Sungwoo Cho <seouk77@gmail.com>
2024-05-31 22:10:15 +0900

    feat: Member 엔티티 수정 (생일, 전화번호, 선호 레스토랑 입력 여부 반영)

--- src/main/java/kr/ac/sejong/ds/palette/member/entity/Member.java
+++ src/main/java/kr/ac/sejong/ds/palette/member/entity/Member.java
@@ -3,8 +3,7 @@ package kr.ac.sejong.ds.palette.member.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import kr.ac.sejong.ds.palette.common.entity.BaseEntity;
-import kr.ac.sejong.ds.palette.couple.entity.Couple;
-import kr.ac.sejong.ds.palette.member.dto.request.MemberNicknameUpdateRequest;
+import kr.ac.sejong.ds.palette.member.dto.request.MemberUpdateRequest;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,23 +31,40 @@ public class Member extends BaseEntity {
     @Enumerated(value = EnumType.STRING)
     private Gender gender;
 
+    @NotNull
+    private String birthOfDate;
+
+    @NotNull
+    private String phone;
+
+    @NotNull
+    private boolean preferenceYn;
+
     // Authentication Token 생성을 위한 생성자
-    public Member(Long id, String email, String password, String nickname, Gender gender) {
+    public Member(Long id, String email, String password, String nickname, Gender gender, String birthOfDate, String phone) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.gender = gender;
+        this.birthOfDate = birthOfDate;
+        this.phone = phone;
+        this.preferenceYn = false;
     }
 
-    public Member(String email, String password, String nickname, Gender gender) {
+    public Member(String email, String password, String nickname, Gender gender, String birthOfDate, String phone) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.gender = gender;
+        this.birthOfDate = birthOfDate;
+        this.phone = phone;
+        this.preferenceYn = false;
     }
 
-    public void updateNickname(final MemberNicknameUpdateRequest memberNicknameUpdateRequest){
-        this.nickname = memberNicknameUpdateRequest.nickname();
+    public void updateMemberInfo(final MemberUpdateRequest memberUpdateRequest){
+        this.nickname = memberUpdateRequest.nickname();
+        this.birthOfDate = memberUpdateRequest.birthOfDate();
+        this.phone = memberUpdateRequest.phone();
     }
 }
